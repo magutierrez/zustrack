0cded6940f459abf1d8e4bf6d6cb1b1c732fdf16
Miguel Angel <miguelangel.gutierrez@deporvillage.com>
2026-03-05 20:29:21 +0100

    feat: added fr and it langs

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -1,7 +1,7 @@
 import { defineRouting } from 'next-intl/routing';
 
 export const routing = defineRouting({
-  locales: ['en', 'es', 'ca'],
+  locales: ['en', 'es', 'ca', 'fr', 'it'],
   defaultLocale: 'en',
   localePrefix: 'always',
   localeCookie: false,
